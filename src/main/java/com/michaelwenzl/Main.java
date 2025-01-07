af256cd8a3dd47422e1d64a5a959616c70a37e4e
Michael Wenzl <michael.wenzl@platomics.com>
2025-01-07 18:04:19 +0100

    Add SDL2 and render a rectangle

---
+++ src/main/java/com/michaelwenzl/Main.java
@@ -0,0 +1,144 @@
+package com.michaelwenzl;
+
+import io.github.libsdl4j.api.event.SDL_Event;
+import io.github.libsdl4j.api.rect.SDL_Rect;
+import io.github.libsdl4j.api.render.SDL_Renderer;
+import io.github.libsdl4j.api.video.SDL_Window;
+
+import static io.github.libsdl4j.api.Sdl.SDL_Init;
+import static io.github.libsdl4j.api.Sdl.SDL_Quit;
+import static io.github.libsdl4j.api.SdlSubSystemConst.SDL_INIT_EVERYTHING;
+import static io.github.libsdl4j.api.error.SdlError.SDL_GetError;
+import static io.github.libsdl4j.api.event.SDL_EventType.SDL_KEYDOWN;
+import static io.github.libsdl4j.api.event.SDL_EventType.SDL_QUIT;
+import static io.github.libsdl4j.api.event.SDL_EventType.SDL_WINDOWEVENT;
+import static io.github.libsdl4j.api.event.SdlEvents.SDL_PollEvent;
+import static io.github.libsdl4j.api.keycode.SDL_Keycode.SDLK_SPACE;
+import static io.github.libsdl4j.api.render.SDL_RendererFlags.SDL_RENDERER_ACCELERATED;
+import static io.github.libsdl4j.api.render.SdlRender.SDL_CreateRenderer;
+import static io.github.libsdl4j.api.render.SdlRender.SDL_RenderClear;
+import static io.github.libsdl4j.api.render.SdlRender.SDL_RenderFillRect;
+import static io.github.libsdl4j.api.render.SdlRender.SDL_RenderPresent;
+import static io.github.libsdl4j.api.render.SdlRender.SDL_SetRenderDrawColor;
+import static io.github.libsdl4j.api.video.SDL_WindowFlags.SDL_WINDOW_RESIZABLE;
+import static io.github.libsdl4j.api.video.SDL_WindowFlags.SDL_WINDOW_SHOWN;
+import static io.github.libsdl4j.api.video.SdlVideo.SDL_CreateWindow;
+import static io.github.libsdl4j.api.video.SdlVideoConst.SDL_WINDOWPOS_CENTERED;
+
+/**
+ * Post to work through
+ * <p>
+ * https://lodev.org/cgtutor/raycasting.html
+ */
+public class Main {
+
+    private static final String APP_NAME = "3D Raycasting";
+
+    private static final int MAP_WIDTH = 24;
+    private static final int MAP_HEIGHT = 24;
+    private static final int SCREEN_WIDTH = 640;
+    private static final int SCREEN_HEIGHT = 480;
+
+    private int[][] worldMap = new int[][]{
+            new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    public static void main(String[] args) {
+        // TODO: IO / Result monad
+
+        // Initialize SDL
+        int result = SDL_Init(SDL_INIT_EVERYTHING);
+        if (result != 0) {
+            throw new IllegalStateException("Unable to initialize SDL library (Error code " + result + "): " + SDL_GetError());
+        }
+
+        // Create and init the window
+        SDL_Window window = SDL_CreateWindow(
+                APP_NAME,
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                SCREEN_WIDTH, SCREEN_HEIGHT,
+                SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+        if (window == null) {
+            throw new IllegalStateException("Unable to create SDL window: " + SDL_GetError());
+        }
+
+        // Create and init the renderer
+        SDL_Renderer renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (renderer == null) {
+            throw new IllegalStateException("Unable to create SDL renderer: " + SDL_GetError());
+        }
+
+        // Set color of renderer to green
+        SDL_SetRenderDrawColor(renderer, (byte) 30, (byte) 31, (byte) 34, (byte) 255);
+
+        // Clear the window and make it all red
+        SDL_RenderClear(renderer);
+
+        // Render the changes above ( which up until now had just happened behind the scenes )
+        SDL_RenderPresent(renderer);
+
+        // Start an event loop and react to events
+        SDL_Event evt = new SDL_Event();
+        boolean shouldRun = true;
+        while (shouldRun) {
+            shouldRun = runRenderLoop(renderer, evt);
+        }
+
+        SDL_Quit();
+    }
+
+    private static boolean runRenderLoop(SDL_Renderer renderer, SDL_Event evt) {
+
+        SDL_SetRenderDrawColor(renderer, (byte) 188, (byte) 190, (byte) 196, (byte) 255);
+
+        var rect = new SDL_Rect();
+        rect.x = 50;
+        rect.y = 50;
+        rect.h = 50;
+        rect.w = 50;
+
+        SDL_RenderFillRect(renderer, rect);
+        SDL_RenderPresent(renderer);
+
+        while (SDL_PollEvent(evt) != 0) {
+            switch (evt.type) {
+                case SDL_QUIT:
+                    return false;
+                case SDL_KEYDOWN:
+                    if (evt.key.keysym.sym == SDLK_SPACE) {
+                        System.out.println("SPACE pressed");
+                    }
+                    break;
+                case SDL_WINDOWEVENT:
+                    System.out.println("Window event " + evt.window.event);
+                default:
+                    break;
+            }
+        }
+        return true;
+    }
+}
